56b2727a77852b96bd7939055f97f475072dfa43
doctorhetfield-cmd <doctorhetfield@gmail.com>
2026-07-16 09:54:56 +0100

    bump version to 2.1.1

--- _meta.lua
+++ _meta.lua
@@ -4,6 +4,6 @@ return {
     name        = "simpleui",
     fullname    = _("Simple UI"),
     description = _([[A simple UI for KOReader]]),
-    version     = "2.1",
+    version     = "2.1.1",
     author      = "Doctor Hetfield",
 }
